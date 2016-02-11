ccb6d86570024df19f1c37198e2c6e16c4707491
Nick Yee <njsyee@gmail.com>
2016-02-10 18:55:50 -0700

    Refactored code

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,20 +1,23 @@
 'use strict';
 $(document).ready(function() {
 
+  var showResults = function(results) {
+    var table = $("#search").find("tbody").empty();
+    results.data.cuis.list.forEach(function(result) {
+      table.append("<tr>");
+      var tr = $("#search").find("tr").last();
+      tr.append('<td>');
+      var td = tr.find("td");
+      debugger
+      td.text(result.cui);
+    });
+  };
+
   $.getJSON('/search', 
     function(results) {
       console.log(results);
-      var table = $("#search").find("tbody").empty();
-      results.data.cuis.list.forEach(function(result) {
-        table.append("<tr>");
-        var tr = $("#search").find("tr").last()
-        tr.append('<td>');
-        var td = tr.find("td")
-        debugger
-        td.text(result.cui);
-      });
 
-
+      showResults(results);
     })
     .fail(function(e) {
       console.log(e);
