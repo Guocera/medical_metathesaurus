f6f2f3dc65e18be387ef2cddab15ffd34f458d84
Nick Yee <njsyee@gmail.com>
2016-02-10 18:13:07 -0700

    displays cui in table

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,9 +1,13 @@
+'use strict';
 $(document).ready(function() {
 
   $.getJSON('/search', 
     function(results) {
       console.log(results);
-      var table = $('#search').find('tbody').empty();
+      var table = $("#search").find("tbody").empty();
+      var tr = table.append("<tr>");
+      tr.append('<td>').text(results);
+
     })
     .fail(function(e) {
       console.log(e);
